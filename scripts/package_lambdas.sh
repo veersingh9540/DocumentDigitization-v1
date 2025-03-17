2f3423e85845aab22960a97b9a6e8d32d5a58b51
Sukhveer Singh <veersingh9540@gmail.com>
2025-03-17 05:07:54 -0500

    Initial commit

--- scripts/package_lambdas.sh
+++ scripts/package_lambdas.sh
@@ -7,7 +7,9 @@ cd src/lambda/document_processor
 pip install -r requirements.txt -t .
 zip -r ../../../document_processor.zip .
 
-# Change to dashboard API directory
+# Change to dashboard API directory here
+
+
 cd ../dashboard_api
 pip install -r requirements.txt -t .
 zip -r ../../../dashboard_api.zip .
