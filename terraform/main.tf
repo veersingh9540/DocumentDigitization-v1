8744c90a220d7bcdaa24eb20d52878f32680d617
Sukhveer Singh <veersingh9540@gmail.com>
2025-03-17 06:07:56 -0500

    working changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,12 +27,12 @@ resource "aws_db_instance" "postgres" {
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "postgres"
-  engine_version       = "13.7"
+  engine_version       = "14"
   instance_class       = "db.t3.micro"
   db_name              = var.db_name
   username             = var.db_username
   password             = var.db_password
-  parameter_group_name = "default.postgres13"
+  parameter_group_name = "default.postgres14"
   skip_final_snapshot  = true
   publicly_accessible  = false
 
@@ -113,123 +113,4 @@ resource "aws_security_group" "dashboard" {
     Name        = "${var.project_name}-dashboard-sg"
     Environment = var.environment
   }
-}
-
-# Lambda for Document Processing
-resource "aws_lambda_function" "document_processor" {
-  function_name = "${var.project_name}-document-processor-${var.environment}"
-  handler       = "index.handler"
-  runtime       = "nodejs14.x"
-  role          = aws_iam_role.lambda_role.arn
-  filename      = "${path.module}/lambda/document_processor.zip"
-  
-  environment {
-    variables = {
-      S3_BUCKET = aws_s3_bucket.documents.bucket
-      DB_ENDPOINT = aws_db_instance.postgres.endpoint
-    }
-  }
-
-  tags = {
-    Name        = "${var.project_name}-document-processor"
-    Environment = var.environment
-  }
-}
-
-# Lambda for Dashboard API
-resource "aws_lambda_function" "dashboard_api" {
-  function_name = "${var.project_name}-dashboard-api-${var.environment}"
-  handler       = "index.handler"
-  runtime       = "nodejs14.x"
-  role          = aws_iam_role.lambda_role.arn
-  filename      = "${path.module}/lambda/dashboard_api.zip"
-  
-  environment {
-    variables = {
-      S3_BUCKET = aws_s3_bucket.documents.bucket
-      DB_ENDPOINT = aws_db_instance.postgres.endpoint
-    }
-  }
-
-  tags = {
-    Name        = "${var.project_name}-dashboard-api"
-    Environment = var.environment
-  }
-}
-
-# IAM Role for Lambda
-resource "aws_iam_role" "lambda_role" {
-  name = "${var.project_name}-lambda-role-${var.environment}"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
-
-  tags = {
-    Name        = "${var.project_name}-lambda-role"
-    Environment = var.environment
-  }
-}
-
-# API Gateway
-resource "aws_api_gateway_rest_api" "dashboard_api" {
-  name        = "${var.project_name}-api-${var.environment}"
-  description = "API Gateway for dashboard"
-}
-
-# API Gateway Resource
-resource "aws_api_gateway_resource" "dashboard_api" {
-  rest_api_id = aws_api_gateway_rest_api.dashboard_api.id
-  parent_id   = aws_api_gateway_rest_api.dashboard_api.root_resource_id
-  path_part   = "dashboard"
-}
-
-# API Gateway Method
-resource "aws_api_gateway_method" "dashboard_api" {
-  rest_api_id   = aws_api_gateway_rest_api.dashboard_api.id
-  resource_id   = aws_api_gateway_resource.dashboard_api.id
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
-# API Gateway Integration
-resource "aws_api_gateway_integration" "dashboard_api" {
-  rest_api_id = aws_api_gateway_rest_api.dashboard_api.id
-  resource_id = aws_api_gateway_resource.dashboard_api.id
-  http_method = aws_api_gateway_method.dashboard_api.http_method
-  
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.dashboard_api.invoke_arn
-}
-
-# API Gateway Deployment
-resource "aws_api_gateway_deployment" "dashboard_api" {
-  depends_on = [
-    aws_api_gateway_integration.dashboard_api
-  ]
-
-  rest_api_id = aws_api_gateway_rest_api.dashboard_api.id
-  stage_name  = var.environment
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-# Lambda Permission for API Gateway
-resource "aws_lambda_permission" "api_gateway" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.dashboard_api.function_name
-  principal     = "apigateway.amazonaws.com"
-  
-  source_arn = "${aws_api_gateway_rest_api.dashboard_api.execution_arn}/*/${aws_api_gateway_method.dashboard_api.http_method}${aws_api_gateway_resource.dashboard_api.path}"
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,11 +8,6 @@ output "rds_endpoint" {
   value       = aws_db_instance.postgres.endpoint
 }
 
-output "api_gateway_url" {
-  description = "The URL of the API Gateway deployment"
-  value       = aws_api_gateway_deployment.dashboard_api.invoke_url
-}
-
 output "ec2_instance_public_ip" {
   description = "The public IP address of the EC2 instance"
   value       = aws_eip.dashboard.public_ip
@@ -26,14 +21,4 @@ output "dashboard_url" {
 output "pgadmin_url" {
   description = "The URL to access pgAdmin"
   value       = "http://${aws_eip.dashboard.public_ip}:8080"
-}
-
-output "lambda_document_processor_name" {
-  description = "The name of the document processor Lambda function"
-  value       = aws_lambda_function.document_processor.function_name
-}
-
-output "lambda_dashboard_api_name" {
-  description = "The name of the dashboard API Lambda function"
-  value       = aws_lambda_function.dashboard_api.function_name
 }
